dc4145d9818a327e147e2b78ee7005be7692695b
Alezandro C.R <79446433+zandrocr@users.noreply.github.com>
2021-06-16 20:07:55 -0300

    mna

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
-const menuMob = document.getElementById("menu-mob");
+const $menu = document.querySelector(".menu")
 
-function toggleMenu(){
-    const mNav = document.getElementById("nav");
-    mNav.classList.toggle("active");
-}
+const $menu1 = document.querySelector(".menu1")
+const $menu2 = document.querySelector(".menu2")
+const $menu3 = document.querySelector(".menu3")
+const $menu4 = document.querySelector('.menu4')
 
-menuMob.addEventListener("click", toggleMwnu)
+const $
