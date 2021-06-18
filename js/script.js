946e35a1e761bd997105edd638c1af62d36c8152
Alezandro C.R <79446433+zandrocr@users.noreply.github.com>
2021-06-18 18:38:39 -0300

    io

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector("#menu");
+
+const menu1 = document.querySelector(".menu1");
+const menu2 = document.querySelector(".menu2");
+const menu3 = document.querySelector(".menu3");
+const menu4 = document.querySelector(".menu4");
+
+menu1.addEventListener('click',function() {
+    menu.classList.remove('in')
+})
