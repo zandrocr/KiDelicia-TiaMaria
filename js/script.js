21e1dc7d8e4a5da4a2192391260df00b2c15395b
Alezandro C.R <79446433+zandrocr@users.noreply.github.com>
2021-06-19 01:49:56 -0300

    fg

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
+var chec = document.querySelector("#check");
 const menu = document.querySelector("#menu");
 
-const menu1 = document.querySelector(".menu1");
-const menu2 = document.querySelector(".menu2");
-const menu3 = document.querySelector(".menu3");
-const menu4 = document.querySelector(".menu4");
-
-menu1.addEventListener('click',function() {
-    menu.classList.remove('in')
-})
+menu.addEventListener ("click", function ck(op){
+    op.checked = false;
+    ck(chec);
+});
