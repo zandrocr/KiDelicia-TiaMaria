968b491aea10110e84ebd1acdaf10ec63818dea9
zandrocr <alezandrocosta@live.com>
2022-07-22 23:39:41 -0300

    update

--- src/pages/Social.jsx
+++ src/pages/Social.jsx
@@ -1,7 +1,7 @@
 //css
 import '../css/social.css'
 //img
-import Table from '../image/table.jpg'
+import Table from '../image/tableFood.jpg'
 //hook
 import React, { useEffect} from 'react'
 import AOS from 'aos';
@@ -18,7 +18,7 @@ const Social = () => {
         <p className="col-10 d-flex align-items-center text-center" data-aos="fade-up" data-aos-duration="2000">
             Vamos logo começar os preparativos para seus maiores e melhores eventos, tudo preparados pelas nossas mãos, ligue para nós, nós conte seus desejos de um dia inesquecível e deixe que cuidamos da cozinha para você.
         </p>
-        <a href="https://api.whatsapp.com/send?phone=5512988656155&text=Ol%C3%A1%2C%20gostaria%20de%20saber%20sobre%20os%20seus%20servi%C3%A7os" target="_blank" className='whats col-3 col-md-1 d-flex justify-content-around' data-aos="fade-down" data-aos-duration="2000">
+        <a href="https://api.whatsapp.com/send?phone=5512988656155&text=Ol%C3%A1%2C%20gostaria%20de%20saber%20sobre%20os%20seus%20servi%C3%A7os" target="_blank" className='whats col-3 col-md-1 d-flex justify-content-around' data-aos="fade-down" data-aos-duration="2000" data-aos-offset="90">
         </a>
         <p className='project'>
             © Projetado e desenvolvido por ZandroCR 2022
